949eb44f2211551c3fa12acbcafcc65c06734f40
Greg D. Hill <greghill@cs.stanford.edu>
2017-02-08 00:08:53 -0600

    add eigen3

--- src/frontend/state_space_model.cc
+++ src/frontend/state_space_model.cc
@@ -1,4 +1,5 @@
 #include "state_space_model.hh"
+#include <Eigen/Dense>
 
 // (throughput, delay) pairs
 std::pair<double, double> StateSpaceModel::query( std::queue<std::pair<double, double>> &/*previous_state*/ )
